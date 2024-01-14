b863ae333d3e38e8b0b7a7e3ed5cd93571951976
Nandan Patil <150545490+nandangitdesk@users.noreply.github.com>
2024-01-14 15:28:22 +0530

    Update script.js
    add character and submit animations

--- Animated form/script.js
+++ Animated form/script.js
@@ -131,3 +131,26 @@ checkbox.addEventListener("click",()=>{
   }
 
 })
+
+
+gsap.set("#eye",{transformorigin:"center"})
+gsap.fromTo("#eye",{scaleY:1},{scaleY:0.3,repeat:-1,yoyo:true,repeatDelay:0.5,ease: "Power2.easeOut"})
+
+gsap.fromTo("#eyebrow",{y:0},{y:-1,repeat:-1,yoyo:true,repeatDelay:0.5,ease: "Power2.easeOut"})
+
+
+//submmit animation
+
+const Button  = document.querySelector("button")
+const tl3 = gsap.timeline({defaults:{duration:0.75,ease: "Power2.easeOut"}});
+
+Button.addEventListener("click",(e)=>{
+  e.preventDefault();
+  tl3.to(".contact-left, .contact-right",{y:30,opacity:0,PointerEvents:"none"})
+  tl3.to("form",{scale:0.8},"<")
+  tl3.fromTo(".submitted",{y:30,opacity:0},{y:0,opacity:1},"<")
+
+  gsap.set("#hand",{transformorigin:"left"})
+  gsap.fromTo("#hand",{rotation:0,y:0},{rotation:-10,y:2,ease:"elastic(3,0.3)",duration:2,delay:1})
+
+})
